a5a3818c1fa8c3ba27df27fc97acce341283956c
faysalzaman <faysalzaman00@gmail.com>
2024-07-25 15:41:06 +0500

    refactor: Initialize user and add marker in map screen

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously, no_leading_underscores_for_local_identifiers
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
